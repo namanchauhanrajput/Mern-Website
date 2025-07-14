4adcd6a1fafaa915d80bc5e4c2262ce2dee996ba
Naman Chauhan <namanrajputnn001@gmail.com>
2025-07-14 21:34:38 +0530

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -24,7 +24,7 @@ export const Login = () => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/login" {
+    const response = await fetch("https://mern-website-five.vercel.app/api/auth/login" {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
