cc3a28be90cff797dfd6389fe9824855d5497834
Naman Chauhan <namanrajputnn001@gmail.com>
2025-07-17 21:47:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use(errorMiddleware);
 // });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
